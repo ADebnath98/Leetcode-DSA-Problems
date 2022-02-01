7aca26a3ae7b49d5cba335058481b764be77e181
Aniket Debnath <36085832+ADebnath98@users.noreply.github.com>
2022-02-01 16:37:23 +0530

    Delete 1281. Subtract the Product and Sum of Digits of an Integer.py

--- 1281. Subtract the Product and Sum of Digits of an Integer.py
+++
@@ -1,8 +0,0 @@
-def subtractProductAndSum(self, n: int) -> int:
-        sum, product = 0, 1
-        while n:
-          digit = n % 10
-          sum += digit
-          product *= digit
-          n //= 10
-        return product - sum
