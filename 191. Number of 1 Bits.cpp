5ea6aa57e85dee2c1ed3713d7fe5d3eb11b17d59
Aniket Debnath <36085832+ADebnath98@users.noreply.github.com>
2022-02-01 16:24:33 +0530

    Add files via upload

---
+++ 191. Number of 1 Bits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        
+        int count = 0;
+        while(n!=0){
+            
+            //checking last bit
+            
+            if(n&1) {
+                
+                count++;
+            }
+            n = n>>1;
+        }
+        return count;
+    }
+};
