d4d8e074880aa6065ded035a997c0e0a4a16041a
Aniket Debnath <36085832+ADebnath98@users.noreply.github.com>
2022-06-21 18:50:55 +0530

    Add files via upload

---
+++ 1642. Furthest Building You Can Reach.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        
+        priority_queue<int> q;
+    
+        int i=0, diff =0; 
+        for(i=0; i<heights.size()-1; i++){ 
+            
+            diff = heights[i+1]-heights[i];
+            
+            if(diff <= 0){
+                continue;
+            }
+
+            bricks -= diff; 
+            q.push(diff);
+            
+            if(bricks < 0){
+                bricks += q.top(); 
+                q.pop(); 
+                ladders--; 
+            }
+
+            if(ladders < 0)
+                break;
+        }
+        return i;
+    }
+};
+            
