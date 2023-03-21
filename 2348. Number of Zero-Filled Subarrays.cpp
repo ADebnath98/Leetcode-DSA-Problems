80ef7342dadd846676f30c2d36718c5aab2f0a3c
Aniket Debnath <36085832+ADebnath98@users.noreply.github.com>
2023-03-21 14:48:12 +0530

    Add files via upload

---
+++ 2348. Number of Zero-Filled Subarrays.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long ans=0, cnt=0;
+        for(int x:nums){
+            if(x)
+              cnt=0;
+            else cnt++;
+            ans+=cnt;  
+        }
+        return ans;
+    }
+};
