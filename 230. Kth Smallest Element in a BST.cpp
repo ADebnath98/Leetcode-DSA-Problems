1fd9fb4dfaf0003744590a0931c8622138591746
Aniket Debnath <36085832+ADebnath98@users.noreply.github.com>
2023-09-09 22:45:54 +0530

    Add files via upload

---
+++ 230. Kth Smallest Element in a BST.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* solve(TreeNode* root,int& k){
+        if(root==NULL) return NULL;
+        TreeNode* left=solve(root->left,k);
+        if(left!=NULL) return left;
+        --k;
+        if(k==0) return root;
+        return solve(root->right,k);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        TreeNode* res=solve(root,k);
+        if(res==NULL) return -1;
+        return res->val;
+    }
+};
